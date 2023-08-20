e853747c1551a4ca921cf56efc032e6016f41d0b
Rafael Mateus <rafaelbmateus.dev@gmail.com>
2023-08-20 18:23:36 -0300

    fix: copy playlist to clipboard

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,5 +1,9 @@
-function copyToClipboard(text) {
-  navigator.clipboard.writeText(text);
+function copyPlaylistToClipboard(href, playlist) {
+  if (href.split("?q=")[1] != ""){
+    href = href.split("?q=")[0] + "?q=" + href.split("?q=")[1];
+  }
+
+  navigator.clipboard.writeText(href);
 
   var element = document.getElementById("toast");
   element.classList.add("show");
