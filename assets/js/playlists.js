52fc1d47414c24d1fa6783ae4e3c5b1b8d5d8c3f
Rafael Mateus <rafaelbmateus.dev@gmail.com>
2023-08-20 11:19:36 -0300

    feat: add playlist home button

--- assets/js/playlists.js
+++ assets/js/playlists.js
@@ -40,7 +40,8 @@ function doPlaylists(playlists, musics) {
         </h2>
         <div id="flush-collapse` + p.id + `" class="accordion-collapse collapse show" aria-labelledby="` + p.id + `" data-bs-parent="#churchs">
           <div class="accordion-body">
-            <button class="btn btn-sm btn-secondary" onclick="copyToClipboard('` + window.location.search.split("?q=")[0] + "?q="+ p.id + `')">
+            <a href="` + window.location.search.split("?q=")[0] + "?q="+ p.id + `">Selecionar</a>
+            <button class="btn btn-sm btn-secondary float-end" onclick="copyToClipboard('` + window.location.search.split("?q=")[0] + "?q="+ p.id + `')">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
                 <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z"/>
               </svg>
